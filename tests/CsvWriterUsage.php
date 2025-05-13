f591c10c99234fd392eaf664c2883999a7255710
jesrel <jletrondo@geniihut.com>
2025-05-13 10:51:09 +0800

    usage update

--- tests/CsvWriterUsage.php
+++ tests/CsvWriterUsage.php
@@ -3,16 +3,19 @@
 require 'vendor/autoload.php';
 
 use Jletrondo\CsvWriter\CsvWriter;
+try {
+    $writer = new CsvWriter('tests/output/output.csv');
+    $writer->setHeader(['first name', 'last name']);
+    $writer->writeHeader();
 
-$writer = new CsvWriter('tests/output/output.csv');
-$writer->setHeader(['first name', 'last name']);
-$writer->writeHeader();
+    $header_rows = [
+        'first name' => ['Jason', 'Alice', 'Dave'],
+        'last name'  => ['Duval', 'Caminos']
+    ];
+    $writer->addRowsFromColumns($header_rows);
 
-$header_rows = [
-    'first name' => ['Jason', 'Alice', 'Dave'],
-    'last name'  => ['Duval', 'Caminos', 'Philips']
-];
-$writer->addRowsFromColumns($header_rows);
-
-$csv = $writer->close();
-echo $csv;
+    $csv = $writer->close();
+    echo $csv;
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
